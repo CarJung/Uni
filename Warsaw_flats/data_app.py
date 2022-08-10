cedf6090f943ed22ce9f4eb6d49594682688197f
CarJung <wrob.kw@gmail.com>
2022-08-10 15:10:23 +0200

    another

--- Warsaw_flats/data_app.py
+++ Warsaw_flats/data_app.py
@@ -7,3 +7,7 @@ import seaborn as sns
 data = pd.read_csv('data/data_clean_removed.csv')
 
 st.title('Warsaw flats prices in July of 2022')
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(data.head(20))
+
