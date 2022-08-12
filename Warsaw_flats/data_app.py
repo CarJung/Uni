09777ba91012ada5650a46563ad2f038181e43a8
CarJung <wrob.kw@gmail.com>
2022-08-12 10:28:27 +0200

    app and nothing

--- Warsaw_flats/data_app.py
+++ Warsaw_flats/data_app.py
@@ -3,11 +3,21 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
 data = pd.read_csv('data/data_clean_removed.csv')
-
+data =data.iloc[:, 1:]
 st.title('Warsaw flats prices in July of 2022')
 
-st.write("Here's our first attempt at using data to create a table:")
+st.write("")
 st.write(data.head(20))
 
+
+st.write("Flats by district")
+
+st.write('Śródmieście')
+box_plot = px.box(data, y="Price")
+st.plotly_chart(box_plot)
+
+st.write("Warsaw map")
+st.map()
