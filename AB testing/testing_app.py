723e9862fe9803b1e6d4b65855985cdd7760d838
KW <wrob.kw@gmail.com>
2022-09-05 18:00:48 +0200

    app

--- AB testing/testing_app.py
+++ AB testing/testing_app.py
@@ -6,5 +6,20 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 import pandas as pd
-import streamlit as str
+import streamlit as st
 
+
+def extract_data(data, selected_columnnames):
+    """Extract columns from the uploaded file by columns selected by the user"""
+    return data[selected_columnnames]
+
+
+st.title("Statistical Testing App")
+
+data = st.file_uploader("Upload a dataset", type=["csv", "txt"])
+
+upload = st.button("Upload")
+if upload:
+    selected_columnnames = st.multiselect("Select columns to analyze", data.columns)
+
+    select_test = st.selectbox("Select a statistical test", ["t-test", "ANOVA"])
