19f1fb80b2bab713e632b2a036912db77eebbe61
KW <wrob.kw@gmail.com>
2022-09-05 19:05:11 +0200

    ttest

--- AB testing/testing_app.py
+++ AB testing/testing_app.py
@@ -9,6 +9,18 @@ import pandas as pd
 import streamlit as st
 
 
+def ttest(data,ispaired = False ,isonesided = False ): 
+    return pg.ttest(data['A'], data['B'], paired=ispaired, alternative='greater' if isonesided else 'two-sided')
+
+def plot_distri(data):
+    fig = plt.figure(figsize=(5, 4))
+    sns.distplot(data['A'], label='A')
+    sns.distplot(data['B'], label='B')
+    plt.xlabel('value')
+    plt.ylabel('Density')
+    plt.legend()
+    return fig
+
 def extract_data(data, selected_columnnames):
     """Extract columns from the uploaded file by columns selected by the user"""
     return data[selected_columnnames]
@@ -16,10 +28,26 @@ def extract_data(data, selected_columnnames):
 
 st.title("Statistical Testing App")
 
-data = st.file_uploader("Upload a dataset", type=["csv", "txt"])
-
-upload = st.button("Upload")
-if upload:
+data = st.file_uploader("Upload a dataset", type="csv")
+if data:
+    data = pd.read_csv(data)
     selected_columnnames = st.multiselect("Select columns to analyze", data.columns)
 
-    select_test = st.selectbox("Select a statistical test", ["t-test", "ANOVA"])
+
+select_test = st.selectbox("Select a statistical test", ["t-test", "ANOVA"])
+
+
+if select_test == "t-test":
+    ispaired = st.checkbox("Is paired")
+    istwosided = st.checkbox("Is two-sided")
+    
+    
+
+run = st.button("Run Test")
+if run:
+    if select_test == "t-test":
+        data = extract_data(data, selected_columnnames)            
+        result = ttest(data, ispaired, istwosided)
+        st.write(result)
+        fig = plot_distri(data)
+        st.pyplot(fig)
